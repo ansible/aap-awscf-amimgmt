8e7293df3514b5a7b18a7ddccebef08dfab6e99c
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2022-08-11 11:57:22 -0400

    Pull out value from IO lib

--- reapsnapshot/main.py
+++ reapsnapshot/main.py
@@ -209,7 +209,7 @@ def main():
     # Reorient stdout back to normal, dump out what it was, and return value to action
     sys.stdout = tmp_stdout
     print(string_stdout.getvalue())
-    print(f"::set-output name=log::{string_stdout}")
+    print(f"::set-output name=log::{string_stdout.getvalue()}")
 
 if __name__ == "__main__":
     main()
