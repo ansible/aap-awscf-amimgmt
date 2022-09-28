375938c1adc0c27d9a08f8746e6bc7cb9d538144
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2022-09-28 14:12:35 -0400

    Unwind return codes
    Signed-off-by: David Schmidt <51931019+schmidtd@users.noreply.github.com>

--- promotetoprod/main.py
+++ promotetoprod/main.py
@@ -24,6 +24,7 @@ def whoami():
     return response
 
 def moveS3s(snapshot_path, snapshot_date, dev_region, prod_region, prod_bucket):
+    return_code = False
     dev_session = boto3.Session(profile_name='dev', region_name=dev_region)
     prod_session = boto3.Session(profile_name='prod', region_name=prod_region)
     resource_map = {}
@@ -31,28 +32,28 @@ def moveS3s(snapshot_path, snapshot_date, dev_region, prod_region, prod_bucket):
         with open("{}/resources-{}.json".format(snapshot_path, snapshot_date), "r") as s3_file:
             resource_text = s3_file.read()
             resource_map = json.loads(resource_text)
+        s3_files = resource_map["s3_files"]
+        dev_client = dev_session.client("s3")
+        prod_client = prod_session.client("s3")
+        for entry in s3_files:
+            if "{}/".format(dev_region) in entry:
+                # Break up an S3 URI into usable bits i.e.
+                # s3://positronic-asimov-us-west-2/cdk/template-development-2022-07-12-10-44-52.json
+                #   bucket --> positronic-asimov-us-west-2
+                #      obj --> cdk/template-development-2022-07-12-10-44-52.json
+                parts = entry.split("s3://")
+                bucket = parts[1].split("/")[0]
+                start = len(bucket) + entry.find(bucket) + 1
+                obj = entry[start:]
+                file = obj.split("/")[1]
+                dev_client.download_file(bucket, obj, file)
+                print("Downloaded {} from {}".format(obj, bucket))
+                prod_client.upload_file(file, prod_bucket, obj)
+                print("Uploaded {} to {}".format(obj, prod_bucket))
+        return_code = True
     except:
         print("Didn't find {}/resources-{}.json!".format(snapshot_path, snapshot_date))
-        exit(False)
-    s3_files = resource_map["s3_files"]
-    dev_client = dev_session.client("s3")
-    prod_client = prod_session.client("s3")
-    for entry in s3_files:
-        if "{}/".format(dev_region) in entry:
-            # Break up an S3 URI into usable bits i.e.
-            # s3://positronic-asimov-us-west-2/cdk/template-development-2022-07-12-10-44-52.json
-            #   bucket --> positronic-asimov-us-west-2
-            #      obj --> cdk/template-development-2022-07-12-10-44-52.json
-            parts = entry.split("s3://")
-            bucket = parts[1].split("/")[0]
-            start = len(bucket) + entry.find(bucket) + 1
-            obj = entry[start:]
-            file = obj.split("/")[1]
-            dev_client.download_file(bucket, obj, file)
-            print("Downloaded {} from {}".format(obj, bucket))
-            prod_client.upload_file(file, prod_bucket, obj)
-            print("Uploaded {} to {}".format(obj, prod_bucket))
-    return
+    return return_code
 
 
 def loginS3Client():
@@ -87,15 +88,15 @@ def main():
         out_file.write(aws_creds_text)
         out_file.close()
 
-    moveS3s(snapshot_path, snapshot_date, dev_region, prod_region, prod_s3_bucket)
+    success = moveS3s(snapshot_path, snapshot_date, dev_region, prod_region, prod_s3_bucket)
 
-    success = False
     # Reorient stdout back to normal, dump out what it was, and return value to action
     sys.stdout = tmp_stdout
     with open(log_filename, "w") as out_file:
         out_file.write(string_stdout.getvalue())
         out_file.close()
     print(f"::set-output name=log::{string_stdout.getvalue()}")
+    print(string_stdout.getvalue())
     exit(not success)
 
 
